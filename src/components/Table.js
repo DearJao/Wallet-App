b6422b5ee8b19c9e15bad6e92fb9bf0b951d9f75
Dear_Jao <jvvr2013@gmail.com>
2022-08-02 14:54:41 -0300

    tabela com historico de despesas

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,7 +3,19 @@ import React, { Component } from 'react';
 export default class Table extends Component {
   render() {
     return (
-      <div>Table</div>
+      <div>
+        <table>
+          <th className="th-table">Descrição</th>
+          <th className="th-table">Tag</th>
+          <th className="th-table">Método de pagamento</th>
+          <th className="th-table">Valor</th>
+          <th className="th-table">Moeda</th>
+          <th className="th-table">Câmbio utilizado</th>
+          <th className="th-table">Valor convertido</th>
+          <th className="th-table">Moeda de conversão</th>
+          <th className="th-table">Editar/Excluir</th>
+        </table>
+      </div>
     );
   }
 }
