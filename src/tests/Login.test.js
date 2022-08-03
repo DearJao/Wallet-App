2aadf427d80bea48ced6f48add96d510179ece2f
Dear_Jao <jvvr2013@gmail.com>
2022-08-03 19:11:19 -0300

    pull pra alterações da main

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import userEvent from '@testing-library/user-event';
 import { screen } from '@testing-library/react';
-import App from '../../App';
-import { renderWithRouterAndRedux } from './renderWith';
+import App from '../App';
+import { renderWithRouterAndRedux } from '../tests/helpers/renderWith';
+
+describe('verify all inputs and elements of the page Login', () => {
+  it('', () => {
+    renderWithRouterAndRedux(<App />);
+
+    const login = screen.getByText(/login/i)
+
+    expect(login).toHaveTextValue(/login/i)
+  })
+})
